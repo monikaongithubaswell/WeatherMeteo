743e957538c3fa3aa9ccd95e58ffe9705c205ce6
monikaongithubaswell <monia.narozna@gmail.com>
2024-01-21 13:17:39 +0100

    added API to search engine

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector("#temp-value");
+  let temperature = response.data.temperature.current;
+  tempElement.innerHTML = Math.round(temperature);
+  let cityElement = document.querySelector("#app-city");
+  cityElement.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "221ba606t5adabe4e4f1018c33o36dff";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#app-city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
